3a05c1d69a339bddff325e8f52e0292bf797a2eb
mosuswalks <mohamedaabdi@yahoo.ca>
2020-03-30 17:35:04 -0400

    Added custom-source-plugin and created graphql query

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,43 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-custom-api`,
+      options: {
+        url: "https://covid19.mathdro.id/api",
+		},
+        rootKey: `worldwide`,
+        schemas: {
+        	confirmed: `	
+            	value: Int
+                detail: String
+          `,
+          recovered: `
+            value: Int
+            detail: String
+          `,
+          deaths: `
+            value: Int
+            detail: String
+          `,
+          dailySummary: `String`,
+          dailyTimeSeries: `
+            pattern: String
+            example: String
+          `,
+          image: `String`,
+          source: `String`,
+          countries: `String`,
+          countryDetail: `
+            pattern: String
+            example: String
+          `,
+          lastUpdate: `String`
+          
+        }
+        
+      }
+    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,59 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+
+
+const IndexPage = () => {
+	const [ confirmed, setConfirmed] = useState()
+	const [ deaths, setDeaths ] = useState()
+	const [ recovered, setRecovered ] = useState()
+	
+	const data = useStaticQuery(graphql`
+		query MyQuery {
+			deaths {
+				value
+			}
+			recovered {
+				value
+			}
+			confirmed {
+				value
+			}
+		}
+	`)
+
+
+
+	useEffect(() => {
+		async function fetchData() {
+			await fetch('https://covid19.mathdro.id/api/countries/canada')
+			.then(response => response.json())
+			.then(resultData => {
+				console.log(resultData.confirmed.value)
+				setConfirmed(resultData.confirmed)
+				setDeaths(resultData.deaths)
+				setRecovered(resultData.recovered)
+			})
+		}
+		fetchData()
+	}, [])
+
+	return(
+		<Layout>
+			<SEO title="Home" />
+
+			<h1>Canadian Covid-19 Stats</h1>
+			<h3>Confirmed Cases: {data.confirmed.value}</h3>
+			<h3>Deaths: {data.deaths.value}</h3>
+			<h3>Recovered: {data.recovered.value}</h3>
+			<Link to="/page-2/">Go to page 2</Link>
+		</Layout>
+	)
+}
+
 
 export default IndexPage
